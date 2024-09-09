f3a61f0b51372c8f437e2afc9d76037e01c33d45
daninithi <danijella544@gmail.com>
2024-09-09 14:17:20 +0530

    new

--- practical8/q2.scala
+++ practical8/q2.scala
@@ -1,16 +1,22 @@
 object number{
     val isMultiplethree:Int=>Boolean=x=>x%3==0
 
+    val isMultiplethreefour:Int=>Boolean=x=>x%3==0 && x%4==0
+
     val isMultiplefive:Int=>Boolean=x=>x%5==0
 
-    val isMultipleboth:Int=>Boolean=x=>x%3==0 && x%5==0 
+    val isMultipleboth:Int=>Boolean=x=>x%3==0 && x%5==0 && x%4==0
+
+    val isMultiplefour:Int=>Boolean=x=>x%4==0
     
     def number(num:Int):Unit={
         num match{
-            case x if isMultipleboth(x)=> println("multiple of both three and five")
+            case x if isMultipleboth(x)=> println("multiple of both three,four and five")
+             case x if isMultiplethreefour(x)=> println("multiple of both three and four")
             case x if isMultiplethree(x)=> println("multiple of three")
-            case x if isMultiplefive(5)=> println("multiple of five")
-            case _=>println("not a multiple of three and five")
+            case x if isMultiplefive(x)=> println("multiple of five")
+            case x if isMultiplefour(x)=> println("multiple of four")
+            case _=>println("not a multiple of three,four and five")
         }
     }
 
